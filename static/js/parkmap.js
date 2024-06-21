977b768cc1f7a0f89d1a42b1bf3c7527a957e8f2
Agnav <104848129+Agnav@users.noreply.github.com>
2024-06-21 22:04:32 +0530

    Update parkmap.js
    final design

--- static/js/parkmap.js
+++ static/js/parkmap.js
@@ -8,13 +8,13 @@ squares.forEach((square) => {
   square.addEventListener("click", () => {
     // Reset background color of all squares
     squares.forEach((otherSquare) => {
-      otherSquare.style.backgroundColor = "rgb(2, 192, 2)";
+      otherSquare.style.backgroundColor = "#f28500";
       otherSquare.style.color = "white";
     });
 
     // Change color of the clicked square to yellow
-    square.style.backgroundColor = "yellow";
-    square.style.color = "black";
+    square.style.backgroundColor = "rgb(2, 192, 2)";
+    square.style.color = "white";
     selectedSlotInput.value = square.textContent;
   });
 });
@@ -25,7 +25,7 @@ document.addEventListener("click", (event) => {
   if (!event.target.classList.contains("square")) {
     // Reset colors to initial values
     squares.forEach((square) => {
-      square.style.backgroundColor = "rgb(2, 192, 2)";
+      square.style.backgroundColor = "#f28500";
       square.style.color = "white";
     });
   }
